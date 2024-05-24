7eebd6b7ffed10ae6d71f661ab9caaee0750bb1a
CiaBroang <cbroang@gmail.com>
2024-05-24 15:48:31 +0200

    Server set up with post and backend fixes

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv"; //hantera miljövariabler från .env-filen
 import { Client } from "pg"; //skapar en PostgreSQL-klient
+import bodyParser from "body-parser" //hantera POST-data
 
 dotenv.config(); // Läs in konfigurationen från .env-filen och gör den tillgänglig via process.env
-
 
 const client = new Client({ //// Skapa en ny instans av en PostgreSQL-klient med anslutningssträngen från .env-filen
   connectionString: process.env.PGURI,
@@ -17,6 +17,7 @@ client.connect().catch((err) => {
 const app = express(); //ny instans av express-applikationen
 
 app.use(cors()); //förhindra CORS-fel
+app.use(bodyParser.json());
 
 app.get("/", async (_req: Request, res: Response) => {
   try {
@@ -34,6 +35,31 @@ app.get("/", async (_req: Request, res: Response) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.post("/messages", async (req: Request, res: Response) => {
+  const { sender_id, receiver_id, message_text, message_image, message_gif } = req.body;
+
+  if (!sender_id || !receiver_id) {
+    return res.status(400).send("sender_id, receiver_id are required");
+  }
+  // Validera för att meddelandet innehåller någonting!
+
+  try {
+    await client.query(
+      "INSERT INTO messages (sender_id, receiver_id, message_text, message_image, message_gif) VALUES ($1, $2, $3, $4, $5)",
+      [sender_id, receiver_id, message_text, message_image, message_gif]
+    );
+    res.status(201).send("Message added");
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error("Error inserting message", err.message);
+      res.status(500).send("Internal Server Error");
+    } else {
+      console.error("Unknown error", err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on http://localhost:8000");
 });
